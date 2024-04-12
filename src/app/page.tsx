26236b489b25511b315bb9e4d44b4c4b897d3d48
pochi-pochi <takahiroa500@outlook.jp>
2024-04-12 22:06:39 +0900

    [add]コンポーネントの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Image from "next/image";
-import { Outline } from "@/app/components/layout/Outline";
+'use client';
 
-export default function Home() {
+import Image from "next/image";
+import Outline from "@/app/components/layout/Outline/Outline";
+import Profile from "./components/layout/Profile/Profile";
+import { NextPage } from "next";
+import React, { useRef, useEffect } from "react";
+
+const Home = () => {
+  const topRef = useRef(null);
+  const outlineRef = useRef(null);
+
+
   return (
     <div>
-      <div>
-
+      <div className="my-10 flex flex-col items-center gap-20">
+        <Outline />
+        <Profile />
       </div>
     </div>
   );
 }
+
+export default Home;
