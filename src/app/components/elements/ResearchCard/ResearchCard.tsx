35bf273b932c877eeb63c3bad7fcd0c3d54c8745
pochi-pochi <takahiroa500@outlook.jp>
2024-04-18 20:48:24 +0900

    [add]altの追加

--- src/app/components/elements/ResearchCard/ResearchCard.tsx
+++ src/app/components/elements/ResearchCard/ResearchCard.tsx
@@ -5,7 +5,7 @@ const ResearchCard = ({src, title, description}: ResearchCardProps) => {
   return(
     <div className="flex justify-center relative w-full static">
       <div>
-        <Image src={src} height={400} width={800}/>
+        <Image src={src} height={400} width={800} alt=""/>
       </div>
       <div className="absolute inset-0 flex flex-col justify-center items-center p-4">
         <div className="bg-black bg-opacity-60 p-3 rounded-lg absolute top-0 right-0">
